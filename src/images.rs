351d7d3475b0bb5ab3440a3d7270b45640b2ffea
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-05-07 13:12:45 +0200

    Refactor modules into indep crates
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ src/images.rs
@@ -0,0 +1,6 @@
+#[derive(Debug)]
+pub struct RawtherapeeImage {
+    pub name: String,
+    pub raw: String,
+    pub sidecar: String,
+}
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod images;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
